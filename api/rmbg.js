ae1a9b50937230290e51620d30f4cc3a8e2bb2dd
huanhoahongso3-collab <144003289+huanhoahongso3-collab@users.noreply.github.com>
2025-11-30 16:20:06 +0700

    Update rmbg.js

--- api/rmbg.js
+++ api/rmbg.js
@@ -22,10 +22,10 @@ export default async function handler(req, res) {
       const blob = new Blob([buffer], { type: "image/png" });
 
       // --- 2. Connect to Gradio Space ---
-      const client = await Client.connect("briaai/BRIA-RMBG-2.0");
+      const client = await Client.connect("briaai/BRIA-RMBG-1.4");
 
       // --- 3. Send image to real server ---
-      const result = await client.predict("/image", {
+      const result = await client.predict("/predict", {
         image: blob,
       });
 
